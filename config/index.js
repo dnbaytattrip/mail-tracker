3f3ca40b3a73bbb988790da1fbb76ed953cf6195
dnbaytattrip <104059752+dnbaytattrip@users.noreply.github.com>
2025-03-15 23:50:43 +0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,5 +3,4 @@ export const id = Cookies.get("id");
 export const adminId = Cookies.get("adminId");
 export const username = Cookies.get("username");
 export const admin = Cookies.get("admin");
-
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
