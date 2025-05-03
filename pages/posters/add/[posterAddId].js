d6374b2390fc95d1300e59c76cd2259e619568be
shovonroy173 <shovonroy2003@gmail.com>
2025-05-03 13:51:27 +0600

    mod: in the add page  links are added test2

--- pages/posters/add/[posterAddId].js
+++ pages/posters/add/[posterAddId].js
@@ -1,27 +1,32 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import Table from "../../../components/Table";
 import { collectionColumn } from "../../../components/Table/columns/collectionColumn";
 import useGetData from "../../../hooks/useGetData";
 import Loader from "../../../components/common/Loader";
-import Modal from "../../../components/common/Modal";
+import Modal from "../../../components/Modal";
 
 function PosterAddPage() {
   const { back, query } = useRouter();
   const { posterAddId } = query;
   const { data, isLoading } = useGetData(`/posters/details/${posterAddId}`);
   const [showModal, setShowModal] = useState(false);
-  const [selectedLink, setSelectedLink] = useState("");
+  // console.log("poster collection", data?.data?.data);
 
-  const { _doc, details } = data ? data?.data?.data : {};
-  const { username, password, posterId, links } = _doc || {};
+  // const { username, password, posterId, links, details } = data
+  //   ? data?.data?.data
+  //   : "";
 
+  const { _doc, details } = data ? data?.data?.data : "";
+
+  const { username, password, posterId, links } = _doc ? _doc : "";
+
+  console.log("poster data _doc:", _doc);
+  // console.log("poster id", posterId);
   const handleAddClick = (link) => {
     setSelectedLink(link);
     setShowModal(true);
   };
-
   return (
     <div className="relative">
       <div className="flex items-center gap-3">
@@ -44,28 +49,54 @@ function PosterAddPage() {
         <div className="mt-7 flex flex-col lg:flex-row gap-5">
           <div className="lg:sticky top-[95px] lg:self-start">
             <div className="text-sm text-custom-gray3 font-semibold min-w-[350px] bg-white p-5 lg:p-6 rounded shadow-md">
+              {/* <h4 className="text-xl text-black">Informations:</h4> */}
+              {/* <div className="mt-3 space-y-3">
+                <p className="grid grid-cols-2">
+                  <span>Username:</span> <span>{username}</span>
+                </p>
+                <p className="grid grid-cols-2">
+                  <span>Password:</span> <span>{password}</span>
+                </p>
+                <p className="grid grid-cols-2">
+                  <span>Poster ID:</span> <span>{posterId}</span>
+                </p>
+              </div> */}
+
               <div className="mt-7">
                 <h4 className="text-xl text-black">Links:</h4>
                 <div className="mt-3 space-y-3">
-                  {links?.map((link, i) => (
-                    <div className="flex gap-5 items-center" key={i}>
-                      <p>{link}</p>
-                      <button
-                        className="bg-green-600 text-xs text-white font-semibold px-2 py-1 rounded"
-                        onClick={() => handleAddClick(link)}
-                      >
-                        Add
-                      </button>
-                    </div>
-                  ))}
+                  {links &&
+                    links?.map((link, i) => (
+                      <div className="flex gap-5 items-center">
+                        <p key={i}>{link}</p>
+                        <button
+                          className="bg-green-600 text-xs text-white font-semibold px-2 py-1 rounded"
+                          onClick={() => handleAddClick(link)}
+                        >
+                          Add
+                        </button>
+                      </div>
+                    ))}
                 </div>
               </div>
             </div>
           </div>
+
+          {/* <div className="lg:flex-1">
+            <div className="bg-white p-4 lg:p-8 rounded shadow-md">
+              <h2 className="text-xl font-semibold mb-5">Collections:</h2>
+              <div className="-mt-10">
+                {details && (
+                  <Table
+                    columnsHeading={collectionColumn}
+                    usersData={details}
+                  />
+                )}
+              </div>
+            </div>
+          </div> */}
         </div>
       </Loader>
-
-      {/* Modal */}
       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
         <h2 className="text-xl font-bold mb-4">Add Link</h2>
         <p className="text-sm text-gray-700">Are you sure you want to add:</p>
